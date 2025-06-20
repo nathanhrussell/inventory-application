ed4dea6bf754940c0b9db65fcc6a73c32d9b0fdf
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-20 15:52:22 +0200

    Populate edit.ejs and modify controller route

--- routes/items.js
+++ routes/items.js
@@ -13,7 +13,18 @@ router.post("/", itemController.createItem);
 
 router.get("/:id", itemController.getItem);
 
-router.get("/:id/edit", (req, res) => res.send("Form to edit item"));
+router.get("/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const itemResult = await db.query("SELECT * FROM items WHERE id = $1", [id]);
+    const categoryResult = await db.query("SELECT * FROM categories ORDER BY name ASC");
+
+    if (itemResult.rows.length === 0) return res.status(404).send("Item not found");
+
+    res.render("items/edit", {
+        item: itemResult.rows[0],
+        categories: categoryResult.rows,
+    });
+});
 
 router.put("/:id", itemController.updateItem);
 
