6036b051f89d1f7262f1ac481c19e79638245912
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-20 15:47:29 +0200

    Populate new.ejs and modify controller route

--- routes/items.js
+++ routes/items.js
@@ -3,11 +3,20 @@ const router = express.Router();
 const itemController = require("../controllers/itemController");
 
 router.get("/", itemController.listItems);
-router.get("/new", (req, res) => res.send("Form to create item"));
+
+router.get("/new", async (req, res) => {
+    const result = await db.query("SELECT * FROM categories ORDER BY name ASC");
+    res.render("items/new", { categories: result. rows});
+});
+
 router.post("/", itemController.createItem);
+
 router.get("/:id", itemController.getItem);
+
 router.get("/:id/edit", (req, res) => res.send("Form to edit item"));
+
 router.put("/:id", itemController.updateItem);
+
 router.delete("/:id", itemController.deleteItem);
 
 module.exports = router;
