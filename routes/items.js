947dd9d3794d05a001f5f32cff8bd43ebb6e20eb
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-06-20 12:19:28 +0200

    Update items.js to use the controller

--- routes/items.js
+++ routes/items.js
@@ -1,32 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const itemController = require("../controllers/itemController");
 
-router.get("/", (req, res) => {
-    res.send("All items");
-});
-
-router.get("/new", (req, res) => {
-    res.send("Form to create item");
-});
-
-router.post("/", (req, res) => {
-    res.send("Create item");
-});
-
-router.get("/:id", (req, res) => {
-    res.send(`View item with ID ${req.params.id}`);
-});
-
-router.get("/:id/edit", (req, res) => {
-    res.send(`Form to edit item with ID ${req.params.id}`);
-});
-
-router.put("/:id", (req, res) => {
-    res.send(`Update item with ID ${req.params.id}`);
-});
-
-router.delete("/:id", (req, res) => {
-    res.send(`Delete item with ID ${req.params.id}`);
-});
+router.get("/", itemController.listItems);
+router.get("/new", (req, res) => res.send("Form to create item"));
+router.post("/", itemController.createItem);
+router.get("/:id", itemController.getItem);
+router.get("/:id/edit", (req, res) => res.send("Form to edit item"));
+router.put("/:id", itemController.updateItem);
+router.delete("/:id", itemController.deleteItem);
 
 module.exports = router;
